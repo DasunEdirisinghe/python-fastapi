8cca92f98069c6e396c74c74af181e4f2888ae1e
DasunEdirisinghe <edirisinghe954@gmail.com>
2022-09-02 23:32:24 +0530

    feat:services & store

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends, status, Response
 from ..database import get_db
-from .. import models, schemas, utils
+from .. import models, schemas, utils, oauth
 from sqlalchemy.orm import Session
 
 router = APIRouter()
@@ -19,4 +19,5 @@ async def login(user_credentials: schemas.Login, db: Session = Depends(get_db)):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Password")
 
-    return {"success": "$#@@B21545#$$%"}
+    access_token = oauth.create_access_token(data={"user_id": user.id})
+    return {"access_token": access_token, "token_type": 'bearer'}
